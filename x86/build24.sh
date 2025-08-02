93f01de78bba71be6dde22c392798a6e856b4b42
wukongdaily <143675923+wukongdaily@users.noreply.github.com>
2025-08-02 19:35:06 +0800

    234

--- x86/build24.sh
+++ x86/build24.sh
@@ -156,7 +156,7 @@ echo "$PACKAGES"
 
 
 # 开始构建
-make image PACKAGES="$PACKAGES" FILES="files"
+make image V=s FORCE=1 PACKAGES="$PACKAGES" FILES="files"
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
